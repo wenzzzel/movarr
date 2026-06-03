536c4922f0c32f03f7b456ccc7b87c4616b89067
wenzzzel <wenzzzel@live.se>
2026-06-03 11:17:37 +0200

    only moving swedish and english subtitles

--- CronScripts/copy-subtitles.sh
+++ CronScripts/copy-subtitles.sh
@@ -4,9 +4,9 @@
 . /app/env-vars.sh
 
 cd "$MOVARR_RADARR_DIRECTORY";
-find . -type f -name "*.srt" -exec cp --parents -v {} "$MOVARR_TDARR_MOVIES_OUTPUT_DIRECTORY" \;
+find . -type f \( -name "*.sv.srt" -o -name "*.swe.srt" -o -name "*.en.srt" -o -name "*.eng.srt" -o -name "*.sv.sdh.srt" -o -name "*.swe.sdh.srt" -o -name "*.en.sdh.srt" -o -name "*.eng.sdh.srt" \) -exec cp --parents -v {} "$MOVARR_TDARR_MOVIES_OUTPUT_DIRECTORY" \;
 chmod -R 777 "$MOVARR_TDARR_MOVIES_OUTPUT_DIRECTORY";
 
 cd "$MOVARR_SONARR_DIRECTORY";
-find . -type f -name "*.srt" -exec cp --parents -v {} "$MOVARR_TDARR_SERIES_OUTPUT_DIRECTORY" \;
+find . -type f \( -name "*.sv.srt" -o -name "*.swe.srt" -o -name "*.en.srt" -o -name "*.eng.srt" -o -name "*.sv.sdh.srt" -o -name "*.swe.sdh.srt" -o -name "*.en.sdh.srt" -o -name "*.eng.sdh.srt" \) -exec cp --parents -v {} "$MOVARR_TDARR_SERIES_OUTPUT_DIRECTORY" \;
 chmod -R 777 "$MOVARR_TDARR_SERIES_OUTPUT_DIRECTORY";
